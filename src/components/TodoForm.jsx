715fb758daf39b0508f28b9aa9a374ad37fe0d37
mohagussetiaone <mohagussetiaone@gmail.com>
2022-06-24 21:20:31 +0700

    Membuat Total List untuk komponen form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -23,10 +23,10 @@ const TodoForm = (props) => {
   // membuat UI
   return (
     <form style={{ margin: "2em 0em" }} onSubmit={onSubmitHandler}>
-      <input type="text" name="todo-baru" id="todoBaru" placeholder="Masukkan Input Anda" style={{ marginRight: "0.5em" }} input={input} onChange={inputHandler} />
+      <input type="text" name="todo-baru" id="todoBaru" placeholder="Masukkan Input Anda" style={{ marginRight: "0.5em" }} value={input} bnb onChange={inputHandler} />
       <button type="submit">Tambah To-Do</button>
       <div className="info">
-        <p>{`Total List ${props.length}`}</p>
+        <p>Total List {props.totalList}</p>
       </div>
     </form>
   );
